4b33a03fa2e249197f369c182705c05927856afd
elegantland <elegant0loneliness@gmail.com>
2024-12-01 15:55:58 +0800

    Update build configuration and dependencies for ESM compatibility

--- docs-source/docs/.vitepress/theme/index.js
+++ docs-source/docs/.vitepress/theme/index.js
@@ -2,7 +2,7 @@ import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
 export default {
-  extends: DefaultTheme,
+  ...DefaultTheme,
   enhanceApp({ app }) {
     // 这里可以添加自定义的 Vue 组件或其他增强功能
   }
